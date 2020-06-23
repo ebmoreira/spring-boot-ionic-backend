1668b92da295c4fc308454d1cf6f50bcf9e835b6
Eduardo Bertin <ebmoreira@gmail.com>
2020-06-23 17:06:54 -0300

    validacao sintatica com Bean Validation

--- src/main/java/com/eduardobertin/cursomc/services/CategoriaService.java
+++ src/main/java/com/eduardobertin/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.eduardobertin.cursomc.domain.Categoria;
+import com.eduardobertin.cursomc.dto.CategoriaDTO;
 import com.eduardobertin.cursomc.repositories.CategoriaRepository;
 import com.eduardobertin.cursomc.services.exceptions.DataIntegrityException;
 import com.eduardobertin.cursomc.services.exceptions.ObjectNotFoundException;
@@ -53,5 +54,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 }
