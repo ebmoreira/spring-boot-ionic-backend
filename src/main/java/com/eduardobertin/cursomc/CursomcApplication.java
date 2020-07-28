7e144fa5ddc13d91c27d1dfa2ce7c4fbf9095410
Eduardo Bertin <ebmoreira@gmail.com>
2020-07-28 12:36:46 -0300

    Enviando imagem via endpoint

--- src/main/java/com/eduardobertin/cursomc/CursomcApplication.java
+++ src/main/java/com/eduardobertin/cursomc/CursomcApplication.java
@@ -1,17 +1,11 @@
 package com.eduardobertin.cursomc;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.eduardobertin.cursomc.services.S3Service;
-
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -19,7 +13,6 @@ public class CursomcApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("/mnt/691D496B6EA2EB79/Curso FullStack/Conteúdo/ana.png");
 	}
 
 }
